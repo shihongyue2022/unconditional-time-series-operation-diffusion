58cdcfd066db5e41ddfc266146b6ee4bdb502c61
shihongyue <shihongyue2022@outlook.com>
2025-09-17 08:43:03 +0900

    chore: commit local changes before merging to fork main

--- src/uncond_ts_diff/dataset.py
+++ src/uncond_ts_diff/dataset.py
@@ -8,6 +8,15 @@ from urllib import request
 from gluonts.dataset.common import load_datasets
 from gluonts.dataset.repository.datasets import get_dataset, get_download_path
 
+from pathlib import Path
+
+from gluonts.dataset.split import split
+from gluonts.dataset.common import (
+    MetaData,
+    TrainDatasets,
+    FileDataset,
+)
+
 default_dataset_path: Path = get_download_path() / "datasets"
 wiki2k_download_link: str = "https://github.com/awslabs/gluonts/raw/b89f203595183340651411a41eeb0ee60570a4d9/datasets/wiki2000_nips.tar.gz"  # noqa: E501
 
@@ -34,3 +43,39 @@ def get_gts_dataset(dataset_name):
         )
     else:
         return get_dataset(dataset_name)
+
+def get_custom_dataset(
+    jsonl_path: Path,
+    freq: str,
+    prediction_length: int,
+    split_offset: int = None,
+):
+    """Creates a custom GluonTS dataset from a JSONLines file and
+    give parameters.
+
+    Parameters
+    ----------
+    jsonl_path
+        Path to a JSONLines file with time series
+    freq
+        Frequency in pandas format
+        (e.g., `H` for hourly, `D` for daily)
+    prediction_length
+        Prediction length
+    split_offset, optional
+        Offset to split data into train and test sets, by default None
+
+    Returns
+    -------
+        A gluonts dataset
+    """
+    if split_offset is None:
+        split_offset = -prediction_length
+
+    metadata = MetaData(freq=freq, prediction_length=prediction_length)
+    test_ts = FileDataset(jsonl_path, freq)
+    train_ts, _ = split(test_ts, offset=split_offset)
+    dataset = TrainDatasets(metadata=metadata, train=train_ts, test=test_ts)
+    return dataset
+    
+    
